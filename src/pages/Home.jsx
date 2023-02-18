86e2d2d968b1f3b81d4b0cfd2596aa6f4c5da64b
Gunny2545 <118515766+Gunny2545@users.noreply.github.com>
2023-02-19 00:40:29 +0700

    commit-message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import MainLayout from "../layouts/MainLayout";
 import ShowProduct from "./ShowProduct";
-import '../pages/pagecss/home.css'
-
+import "../pages/pagecss/home.css";
 
 const Home = () => {
   return (
     <MainLayout>
       <div className="image-grid">
-         <img className="image-grid-col-2 image-grid-row-2" src="https://i.pinimg.com/564x/52/05/e4/5205e44a326c509a7e20b1baefbc7aed.jpg" alt="architecture" />
-         <img src="https://i.pinimg.com/564x/52/05/e4/5205e44a326c509a7e20b1baefbc7aed.jpg" alt="architecture" />
-         <img src="https://i.pinimg.com/564x/52/05/e4/5205e44a326c509a7e20b1baefbc7aed.jpg" alt="architecture" />
-         <img src="https://i.pinimg.com/564x/52/05/e4/5205e44a326c509a7e20b1baefbc7aed.jpg" alt="architecture" />
-         <img src="https://i.pinimg.com/564x/52/05/e4/5205e44a326c509a7e20b1baefbc7aed.jpg" alt="architecture" />
+        <img
+          className="image-grid-col-2 image-grid-row-2"
+          src="https://i.pinimg.com/564x/52/05/e4/5205e44a326c509a7e20b1baefbc7aed.jpg"
+          alt="architecture"
+        />
+        <img
+          src="https://i.pinimg.com/564x/52/05/e4/5205e44a326c509a7e20b1baefbc7aed.jpg"
+          alt="architecture"
+        />
+        <img
+          src="https://i.pinimg.com/564x/52/05/e4/5205e44a326c509a7e20b1baefbc7aed.jpg"
+          alt="architecture"
+        />
+        <img
+          src="https://i.pinimg.com/564x/52/05/e4/5205e44a326c509a7e20b1baefbc7aed.jpg"
+          alt="architecture"
+        />
+        <img
+          src="https://i.pinimg.com/564x/52/05/e4/5205e44a326c509a7e20b1baefbc7aed.jpg"
+          alt="architecture"
+        />
       </div>
       <ShowProduct />
     </MainLayout>
